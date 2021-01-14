5a5e1f1d6293e244b1d061c0232bee47a1d9bb32
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-01-13 17:44:41 -0800

    added a switch case function and prompt functions for each employee class

--- Develop/app.js
+++ Develop/app.js
@@ -9,10 +9,148 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const allMembers = []
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: `What is your name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `What is your ID?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What is your email?`,
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: `What is your office number?`,
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: `What is your name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `What is your ID?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What is your email?`,
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: `What is your GitHub?`,
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: `What is your name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `What is your ID?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What is your email?`,
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: `Where do you go to school?`,
+    }
+]
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: `Which employee would you like to add?`,
+            choices: ["Manager", "Engineer", "Intern", "Generate team"]
+        }
+
+
+    ]).then(response => {
+        console.log(response);
+        switch (response.employeeType) {
+            case 'Manager':
+                managerPrompt();
+                break;
+            case 'Engineer':
+                engineerPrompt();
+                break;
+            case 'Intern':
+                internPrompt();
+                break;
+            default:
+                console.log("Generating team HTML");
+                createHtml();
+                break;
+        }
+    })
+}
+
+const managerPrompt = () => {
+    inquirer.prompt(managerQuestions)
+        .then(managerResponse => {
+            const newManager = new Manager(managerResponse.name, managerResponse.id, managerResponse.email, managerResponse.officeNumber)
+            console.log("Manager info", newManager);
+            allMembers.push(newManager);
+            promptUser();
+        })
+}
+
+const engineerPrompt = () => {
+    inquirer.prompt(engineerQuestions)
+        .then(engineerResponse => {
+            const newEngineer = new Engineer(engineerResponse.name, engineerResponse.id, engineerResponse.email, engineerResponse.github);
+            console.log("Engineer info", newEngineer);
+            allMembers.push(newEngineer);
+            promptUser();
+        })
+}
+
+const internPrompt = () => {
+    inquirer.prompt(internQuestions)
+        .then(internResponse => {
+            const newIntern = new Intern(internResponse.name, internResponse.id, internResponse.email, internResponse.school);
+            console.log("Intern info", newIntern);
+            allMembers.push(newIntern);
+            promptUser();
+        })
+}
+
+const createHtml = () => {
+    console.log("All Members are", allMembers);
+    console.log(outputPath);
+    fs.writeFile(outputPath, render(allMembers), (err) => {
+        if (err) throw err;
+        console.log('The team profile has been generated!');
+    });
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +171,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+promptUser();
